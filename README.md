78f1fa27de3379add0fb4759b026b1693458491a
Riya Kanabar <riyamkanabar1608@gmail.com>
2020-06-17 21:01:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pencil-Reader
+An application to convert the handwritten data to e-data so that can be used more efficiently.
+
+# Installation
+to install the requisites execute the following commands -
+
+-> npm install 
+
+-to install node package manager
+
+-> npm init 
+
+-it will generate the package.json which will hold all of our dependencies that we will install in for our project
+
+-> npm install ejs express multer tesseract.js@2.0.0-alpha.15
+
+-to install the dependencies
+
+-> npm install --save-dev nodemon
+
+-to automatically reload the saved changes
+
+# Run the application
+-> npm start
---
+++ app.js
@@ -0,0 +1,69 @@
+// imports
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+
+const fs = require("fs");
+const multer = require('multer');
+const {TesseractWorker} = require('tesseract.js');
+const worker = new TesseractWorker();
+
+//storage
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req,file,cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage: storage}).single('avatar');
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//routes
+app.get('/', (req,res) => {
+    res.render('index');
+});
+app.get('/choosefile', (req, res) => {
+    res.render('choosefile');
+   });
+app.get('/templatechoose', (req, res) => {
+    res.render('templatechoose');
+   });
+app.get('/convertBlank', (req, res) => {
+    res.render('convertBlank');
+});
+app.post('/upload', (req,res) => {
+    upload(req,res, err => {
+        fs.readFile(`./uploads/${req.file.originalname}`, (err,data) => {
+            if(err) return console.log('An error occured', err);
+
+            worker
+            .recognize(data, "eng", {tessjs_create_pdf: '1'})
+            .progress(progress => {
+                console.log(progress);
+            })
+            .then(result => {
+                res.send(result.text);
+                //res.redirect('/download');
+            })
+            .finally(() => worker.terminate());
+        });
+        //console.log(req.file);
+    });
+});
+
+app.get("/download", (req,res) => {
+    const file = `${__dirname}/tesseract.js-ocr-result.pdf`;
+    res.download(file);
+});
+
+//start up our server
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Hey Im running on port ${PORT}`));
